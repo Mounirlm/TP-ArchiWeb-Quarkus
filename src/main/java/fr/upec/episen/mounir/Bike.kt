12c6b70e5b7543a5eeeb70407edad78e00c08017
Mounir <mlemharzi@yahoo.fr>
2021-02-13 19:38:52 +0100

    TP noté quarkus bike bonus conversion de la classe bike en kotlin

---
+++ src/main/java/fr/upec/episen/mounir/Bike.kt
@@ -0,0 +1,11 @@
+package fr.upec.episen.mounir
+
+import lombok.Data
+
+@Data
+class Bike {
+    private val model: String? = null
+    private val price: Int? = null
+    private val identification: String? = null
+    private val owner: String? = null
+}
